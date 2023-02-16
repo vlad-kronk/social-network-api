bfaf330875dd584b5a899807797143c1a74dcde2
Jacob Meyers <jacobmeyers6@outlook.com>
2023-02-15 18:43:05 -0800

    why is the remove friend not working please im begging you

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,8 +34,63 @@ function updateUser(req, res) {
       { _id: req.params.id },
       { username: req.body.username }
    )
-      .then((user) => res.json(`User ${user._id} has been updated.`))
+      .then((user) => {
+         !user
+            ? res.status(404).json({ message: `No user with that ID` })
+            : res.json({ message: `User ${user._id} has been updated.` })
+      })
       .catch((err) => res.status(500).json(err));
+}
+
+// delete a user by id
+// also deletes user's associated thoughts
+function deleteUser(req, res) {
+   User.findOneAndDelete({ _id: req.params.id })
+      .then((user) =>
+         !user
+            ? res.status(404).json({ message: 'No user with that ID' })
+            : Thought.deleteMany({ _id: { $in: user.thoughts } })
+      )
+      .then(() => res.json({ message: 'User and associated thoughts deleted!' }))
+      .catch((err) => console.log(err)); // when i do a res.status(500).json(err) the server quits when the error occurs
+}
+
+// add a friend to the user's friends list
+function addFriend(req, res) {
+   User.findOne({ _id: req.params.fid })
+      .then((friend) =>
+         !friend
+            ? res.status(404).json({ message: 'No user with that ID' })
+            : User.findOneAndUpdate(
+               { _id: req.params.id },
+               { $addToSet: { friends: req.params.fid } }
+            ).then((user) =>
+               !user
+                  ? res.status(404).json({ message: 'No user with that ID' })
+                  : res.json(user)
+            ).catch((err) => { res.status(500).json(err); console.log(err) })
+      ).catch((err) => {
+         res.status(500).json(err);
+         console.log(err);
+      })
+}
+
+// remove a friend from the user's friends list
+function removeFriend(req, res) {
+   User.findOneAndUpdate(
+      { _id: req.params.id },
+      { $pull: { friends: { _id: req.params.fid } } },
+      { runValidators: true, new: true }
+   )
+      .then((user) => {
+         !user
+            ? res.status(404).json({ message: 'No user with that ID' })
+            : res.json(user)
+      })
+      .catch((err) => {
+         res.status(500).json(err);
+         console.log(err);
+      });
 }
 
 module.exports = {
@@ -43,4 +98,7 @@ module.exports = {
    getSingleUser,
    createUser,
    updateUser,
+   deleteUser,
+   addFriend,
+   removeFriend,
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,10 +4,14 @@ const {
    getSingleUser,
    createUser,
    updateUser,
+   deleteUser,
+   addFriend,
+   removeFriend,
 } = require('../../controllers/userController');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
-router.route('/:id').get(getSingleUser).put(updateUser);
+router.route('/:id').get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route('/:id/friends/:fid').post(addFriend).delete(removeFriend);
 
 module.exports = router;
